11710be9b767360176fa99abdea1e39ed27ef948
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-08-08 19:25:52 +0000

    endpoint done

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,6 +7,25 @@ from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
+#get all users in db
+@api.route('/users', methods=['GET'])
+def getAllPeople():
+    people_query = User.query.all()
+    all_people = list(map(lambda x: x.serialize(), people_query))
+    return jsonify(all_people)
+
+@api.route('/users/<int:id>', methods=['GET'])
+def getUser(id):
+    people_query = User.query.get(id)
+    return jsonify(people_query.serialize())
+
+@api.route('/register', methods=['POST'])
+def createUser():
+    info_request = request.get_json()
+    newUser = User(id = info_request['id'], email = info_request['email'], password = info_request['password'], is_active = info_request['is_active'])
+    db.session.add(newUser)
+    db.session.commit()
+    return "Usuario creado", 201
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
