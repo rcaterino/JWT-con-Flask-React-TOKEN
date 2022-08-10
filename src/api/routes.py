da21b8ecb1e318fdb1ac08a7607452d86f0aca82
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2022-08-10 03:36:34 +0000

    Implementando JWT en api y creando endpoint

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,9 +4,39 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
+#---------------------------------------------------------------------------
+# Crea una ruta para autenticar a los usuarios y devolver el token JWT.
+# La función create_access_token() se utiliza para generar el JWT.
+@app.route("/token", methods=["POST"])
+def create_token():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    user = User.filter.query(username=username, password=password).first()
+    if User is None:
+          # el usuario no se encontró en la base de datos
+        return jsonify({"msg": "Bad username or password"}), 401
+    # crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+#----------------------------------------------------------------------------
+# Protege una ruta con jwt_required, bloquea las peticiones
+# sin un JWT válido presente.
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.filter.get(current_user_id)
+    
+    return jsonify({"id": user.id, "username": user.username }), 200
+
+#----------------------------------------------------------------------------
 #get all users in db
 @api.route('/users', methods=['GET'])
 def getAllPeople():
@@ -14,11 +44,15 @@ def getAllPeople():
     all_people = list(map(lambda x: x.serialize(), people_query))
     return jsonify(all_people)
 
+#----------------------------------------------------------------------------
+#get only one user in db
 @api.route('/users/<int:id>', methods=['GET'])
 def getUser(id):
     people_query = User.query.get(id)
     return jsonify(people_query.serialize())
 
+#----------------------------------------------------------------------------
+#create a new user in db
 @api.route('/register', methods=['POST'])
 def createUser():
     info_request = request.get_json()
@@ -27,6 +61,7 @@ def createUser():
     db.session.commit()
     return "Usuario creado", 201
 
+#----------------------------------------------------------------------------
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,23 +4,76 @@ import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <>
+      <h1 className="text-center">Bienvenido</h1>
+      <div className="container">
+        <form class="row-sm needs-validation" novalidate="">
+          <div className="container">
+            <div class="col-sm">
+              <label for="validationCustomUsername" class="form-label">
+                Username
+              </label>
+              <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">
+                  @
+                </span>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="validationCustomUsername"
+                  aria-describedby="inputGroupPrepend"
+                  required=""
+                />
+                <div class="invalid-feedback">Please write a username.</div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div class="col-sm">
+              <label for="validationCustomPassword" class="form-label">
+                Password
+              </label>
+              <div class="input-group has-validation">
+                <span class="input-group-text" id="inputGroupPrepend">
+                  **
+                </span>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="validationCustomPassword"
+                  aria-describedby="inputGroupPrepend"
+                  required=""
+                />
+                <div class="invalid-feedback">Please write a password.</div>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div class="col-sm">
+              <div class="form-check">
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="invalidCheck"
+                  required=""
+                />
+                <label class="form-check-label">Hold my session open</label>
+              </div>
+            </div>
+          </div>
+          <div className="container">
+            <div class="col-sm">
+              <button class="btn btn-primary" type="submit">
+                Submit form
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
